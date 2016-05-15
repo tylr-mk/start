fb6d822d0900bb950b819cbd72e5be725054625d
tylr-mk <tylr.mk@gmail.com>
2016-05-15 12:43:18 +0100

    Branch / Merge Test
    I don't really know if I like this Github Destkop client

--- test-folder/hello.js
+++ test-folder/hello.js
@@ -1,2 +1,5 @@
-console.log('So that\'s basic backend JavaScript rendering made available via node!' );
+console.log('So that\'s basic backend 
+this is really  annoying to play around in 
+windows.... but at least I'm not onto 
+instalign complicated packages and platforms yet' );
 
